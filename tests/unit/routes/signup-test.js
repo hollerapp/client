ebcbd4e39a8aca8107b72e11d5b64d7f759de178
Jeffrey Canty <self@cantyjeffrey.cc>
2015-01-28 20:09:16 -0600

    Update test naming for signup route

--- tests/unit/routes/signup-test.js
+++ tests/unit/routes/signup-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleFor('route:signin', 'SigninRoute', {
+moduleFor('route:signup', 'SignupRoute', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
